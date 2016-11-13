08af683a4d148b0c988bfc65c9c3996c3ed27528
Marcelo Gornstein <marcelog@gmail.com>
2016-11-13 17:48:37 -0300

    fixing typo

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
-AWS_DEFAULT_REGION=us-west-2 \
-AWS_REGION=us-west-2 \
+AWS_DEFAULT_REGION=your_region \
+AWS_REGION=your_region \
 AWS_ACCESS_KEY_ID=your_key \
 AWS_SECRET_ACCESS_KEY=your_secret \
 node index.js
